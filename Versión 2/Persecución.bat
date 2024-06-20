24392e8f8eecdbcbcb43a4f28bb7f07b47fb2134
GeGeFe <gabrielgf2002@yahoo.com.ar>
2024-06-19 22:24:43 -0300

    Se identifica SO si se ejecuta el bat o el sh.

--- Versión 2/Persecución.bat
+++ Versión 2/Persecución.bat
@@ -2,6 +2,7 @@
 REM Para poder reproducir sonidos instalar ffmpeg desde https://github.com/BtbN/FFmpeg-Builds/releases
 REM Instalarlo siguiendo el este tutorial: https://es.wikihow.com/instalar-FFmpeg-en-Windows
 set contenidocelestia=%~1
+set so="Windows"
 c:
 cd \
 cd "Program Files (x86)
--- Versión 2/Persecución.sh
+++ Versión 2/Persecución.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export contenidocelestia=$1
+export so="Linux"
 celestia-qt5 -u Persecucion.celx

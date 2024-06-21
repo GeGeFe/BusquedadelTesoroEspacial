481cdb66f5b2c05838d83f4ab1c97e3d0c621ee0
GeGeFe <gabrielgf2002@yahoo.com.ar>
2024-06-21 16:54:56 -0300

    El script de linux ahora prueba con diferentes ejecutables de celestia (por si no es celestia-qt5).

--- Versión 2/Persecución.sh
+++ Versión 2/Persecución.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 export contenidocelestia=$1
 export so="Linux"
-celestia-qt5 -u Persecucion.celx
+
+nombres_executables=("celestia" "celestia-qt5" "celestia-qt")
+for nombre in "${nombres_executables[@]}"; do
+    # Intenta ejecutar el programa con el nombre actual
+    $nombre -u Persecucion.celx > /dev/null 2>&1
+done

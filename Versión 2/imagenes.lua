64c5b60a8c59380d33ab1d50c8b3a1fb85eb142a
GeGeFe <gabrielgf2002@yahoo.com.ar>
2024-06-24 11:22:50 -0300

    Dibujos para personalizar.

---
+++ Versión 2/imagenes.lua
@@ -0,0 +1,36 @@
+
+-- Agregar aquí entre las comillas la ubicación de inicio (Navegación/Copy URL en Celestia)
+-- Comentar la línea si no se quiere especificar una ubicación inicial.
+sinsonido = true
+noimagen = false
+nombresalazar = "profesioneschistosas.lua"
+
+ubicacioninicial = "cel://Follow/Sol:Earth/2024-06-24T14:05:47.89155Z?x=Gj5unG+lDw&y=uRd0SeIl/////////////w&z=AKOKHX1e9/7//////////w&ow=0.029538626&ox=-0.010327371&oy=-0.99950945&oz=0.0013025872&select=Sol:Earth&fov=29.356295&ts=1&ltd=0&p=0&rf=71161735&nrf=255&lm=2048&tsrc=0&ver=3"
+objetodeinicio = "Tierra"
+
+introduccion = {
+    pausa = "1",
+    texto = "Probando",
+    imagen = "",
+    sonido = ""
+}
+
+final = {
+    pausa = "",
+    texto = "",
+    imagen = "",
+    sonido = "",
+    efecto = "fuegosartificiales"
+}
+
+recorrido = {
+    
+    {   objetivo = "Luna",
+        pistas = {
+            { texto = "Probando ir a la luna", personaje = "Diana_dibujo.png" },
+        },
+        fallos = { "¡Luna!" },
+        felicitacion = "Bien"
+    },
+
+}

d1505ea8718929416467a556de03e0c2a996ed5e
GeGeFe <gabrielgf2002@yahoo.com.ar>
2024-11-21 11:06:17 -0300

    Agregué los ejecutables que figuran en documentación de Celestia para poder encontrarlo.

--- Version_2/Persecución.sh
+++ Version_2/Persecución.sh
@@ -2,8 +2,12 @@
 export contenidocelestia=$1
 export so="Linux"
 
-nombres_executables=("celestia" "celestia-qt5" "celestia-qt")
+nombres_executables=("celestia" "celestia-qt5" "celestia-qt" "celestia-qt6" "celestia-gtk" "celestia-sdl" "celestia-win")
 for nombre in "${nombres_executables[@]}"; do
     # Intenta ejecutar el programa con el nombre actual
-    $nombre -u Persecucion.celx > /dev/null 2>&1
+    if $nombre -u Persecucion.celx > /dev/null 2>&1; then
+        break; # Sale del bucle si logra ejecutar Celestia
+    else
+        echo "No se pudo encontrar el ejecutable de Celestia"
+    fi
 done

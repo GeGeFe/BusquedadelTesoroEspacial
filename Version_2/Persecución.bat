877dc712331422eb9679e64a672d9abbe37afa3f
GeGeFe <gabrielgf2002@yahoo.com.ar>
2024-11-21 11:07:36 -0300

    Error de redacción.

--- Version_2/Persecución.bat
+++ Version_2/Persecución.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Para poder reproducir sonidos instalar ffmpeg desde https://github.com/BtbN/FFmpeg-Builds/releases
-REM Instalarlo siguiendo el este tutorial: https://es.wikihow.com/instalar-FFmpeg-en-Windows
+REM Instalarlo siguiendo el tutorial: https://es.wikihow.com/instalar-FFmpeg-en-Windows
 set contenidocelestia=%~1
 set so="Windows"
 c:
